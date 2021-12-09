232b2abad8862a438a539d77515c0dccc982c465
Enguerran <enguerran.huby@aroed.fr>
2021-12-10 00:43:07 +0100

    Partie 4 - Consommer cette API

--- src/main/java/fr/lernejo/navy_battle/check/Check.java
+++ src/main/java/fr/lernejo/navy_battle/check/Check.java
@@ -1,5 +1,7 @@
 package fr.lernejo.navy_battle.check;
 
+import java.util.Objects;
+
 public class Check {
 
     public boolean isStringInt(String string){
@@ -13,4 +15,21 @@ public class Check {
         return is_Int;
     }
 
+    public String convertArgPort(String string){
+        final String[] myport = new StringBuilder(string).reverse().toString().split("");
+        final StringBuilder theport = new StringBuilder();
+        int i = 0;
+        while(!Objects.equals(myport[i], ":") && !Objects.equals(myport[i], "\0"))
+        {
+            theport.append(myport[i]);
+            i++;
+        }
+        return theport.reverse().toString();
+    }
+
+    public void displayHelp(){
+        System.out.println("Pour lancer le programme :");
+        System.out.println("1. Arg: <port>");
+        System.out.println("2. Args: <port> <url:port>");
+    }
 }
