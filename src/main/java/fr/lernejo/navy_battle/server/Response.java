3b9649355a37552fa56a100f4656365b7b5de4b3
Enguerran <enguerran.huby@aroed.fr>
2021-12-09 23:46:16 +0100

    Partie 3

---
+++ src/main/java/fr/lernejo/navy_battle/server/Response.java
@@ -0,0 +1,30 @@
+package fr.lernejo.navy_battle.server;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Response{
+
+    public void basic_response(HttpExchange exchange, int code, String message) throws IOException {
+        exchange.sendResponseHeaders(code, message.length());
+        try(final OutputStream os = exchange.getResponseBody()){
+            os.write(message.getBytes());
+        }
+    }
+
+    public void json_response(HttpExchange exchange, int code, String id, String message) throws IOException {
+        final String body = "{\n\t\"id\": \""
+            + id
+            + "\",\n\t\"url\": \"http://localhost:"
+            + exchange.getLocalAddress().getPort()
+            + "\",\n\t\"message\": \""
+            + message
+            + "\"\n}";
+        exchange.sendResponseHeaders(code, body.length());
+        try(final OutputStream os = exchange.getResponseBody()){
+            os.write(body.getBytes());
+        }
+    }
+}
